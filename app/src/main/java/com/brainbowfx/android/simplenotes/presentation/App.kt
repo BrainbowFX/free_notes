ac90b1104c7b281cfe5920f166e86e0f6db35485
BrainbowFX <brainbowfx@gmail.com>
2019-03-04 13:51:31 +0500

    Added: application class Modified: Application class name in AndroidManifest

---
+++ app/src/main/java/com/brainbowfx/android/simplenotes/presentation/App.kt
@@ -0,0 +1,26 @@
+package com.brainbowfx.android.simplenotes.presentation
+
+import android.app.Application
+import com.brainbowfx.android.simplenotes.di.components.ApplicationComponent
+import com.brainbowfx.android.simplenotes.di.modules.AppModule
+import com.brainbowfx.android.simplenotes.di.modules.DatabaseModule
+import com.brainbowfx.android.simplenotes.di.modules.SpeechRecognitionModule
+
+class App: Application() {
+
+    companion object {
+        lateinit var Instance: App
+    }
+
+    lateinit var appComponent: ApplicationComponent
+
+    override fun onCreate() {
+        super.onCreate()
+        Instance = this
+        appComponent = DaggerApplicationComponent.builder()
+            .appModule(AppModule(this))
+            .databaseModule(DatabaseModule())
+            .speechRecognitionModule(SpeechRecognitionModule())
+            .build()
+    }
+}
