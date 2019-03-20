f2cd8414e271c7341233006e18e12638a9fa5212
BrainbowFx <brainbowfx@gmail.com>
2019-03-20 13:29:42 +0500

    Added: ActivitySubcomponent and ActivityPerInstanceSubcomponent initialization and destruction methods inside App

--- app/src/main/java/com/brainbowfx/android/simplenotes/presentation/App.kt
+++ app/src/main/java/com/brainbowfx/android/simplenotes/presentation/App.kt
@@ -1,11 +1,12 @@
 package com.brainbowfx.android.simplenotes.presentation
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatActivity
+import com.brainbowfx.android.simplenotes.di.components.ActivityPerInstanceSubcomponent
+import com.brainbowfx.android.simplenotes.di.components.ActivitySubcomponent
 import com.brainbowfx.android.simplenotes.di.components.ApplicationComponent
 import com.brainbowfx.android.simplenotes.di.components.DaggerApplicationComponent
-import com.brainbowfx.android.simplenotes.di.modules.AppModule
-import com.brainbowfx.android.simplenotes.di.modules.DatabaseModule
-import com.brainbowfx.android.simplenotes.di.modules.FilesModule
+import com.brainbowfx.android.simplenotes.di.modules.*
 
 class App: Application() {
 
@@ -14,6 +15,8 @@ class App: Application() {
     }
 
     lateinit var appComponent: ApplicationComponent
+    var activitySubcomponent: ActivitySubcomponent? = null
+    var activityPerInstanceSubcomponent: ActivityPerInstanceSubcomponent? = null
 
     override fun onCreate() {
         super.onCreate()
@@ -23,5 +26,25 @@ class App: Application() {
             .databaseModule(DatabaseModule())
             .filesModule(FilesModule())
             .build()
+    }
+
+    fun plusActivitySubcomponent(): ActivitySubcomponent =
+        activitySubcomponent ?: appComponent.activitySubcomponent()
+            .mappersModule(MappersModule())
+            .speechRecognitionModule(SpeechRecognitionModule())
+            .dateModule(DateModule())
+            .build().also { activitySubcomponent = it }
+
+    fun plusActivityPerInstanceSubcomponent(activity: AppCompatActivity): ActivityPerInstanceSubcomponent? =
+            activityPerInstanceSubcomponent ?: activitySubcomponent?.activityPerInstanceSubcomponent()
+                ?.activityModule(ActivityModule(activity))
+                ?.build().also { activityPerInstanceSubcomponent = it  }
+
+    fun clearActivitySubcomponent() {
+        activitySubcomponent = null
+    }
+
+    fun clearActivityPerInstanceSubcomponent() {
+        activityPerInstanceSubcomponent = null
     }
 }
